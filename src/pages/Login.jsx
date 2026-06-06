5a0fe870471f4dac4d0ff2259f13bea484c72745
Lucas Marques <lucasmarques630@gmail.com>
2026-06-06 09:23:13 -0300

    Ambiente produção

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,9 +4,7 @@ import { supabase } from '../services/supabase'
 export default function Login() {
 
   useEffect(() => {
-
     async function verificar() {
-
       const {
         data: { session }
       } = await supabase.auth.getSession()
@@ -15,33 +13,34 @@ export default function Login() {
     }
 
     verificar()
-
   }, [])
 
   async function loginGoogle() {
+    // Captura dinamicamente a URL de onde o site está rodando
+    const URL_ATUAL = window.location.origin 
 
-    const { data, error } =
-      await supabase.auth.signInWithOAuth({
-        provider: 'google',
-        options: {
-          redirectTo: 'http://localhost:5173/admin'
-        }
-      })
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        // Agora funciona tanto em localhost quanto na Vercel!
+        redirectTo: `${URL_ATUAL}/admin` 
+      }
+    })
 
-    console.log('DATA:', data)
-    console.log('ERROR:', error)
+    if (error) {
+      console.error('Erro no login:', error.message)
+    } else {
+      console.log('Redirecionando...', data)
+    }
   }
 
   return (
     <div className="container">
       <div className="login-card">
-
         <h1>Corrida São Silvestre</h1>
-
         <button onClick={loginGoogle}>
           Entrar com Google
         </button>
-
       </div>
     </div>
   )
